44235cee56b9a6204792baa6b057f6a21cfe3f47
bplok20010 <nobo.zhou@foxmail.com>
2019-03-17 21:48:46 +0800

    Modified   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import Form from './Form';
-import FormValue from './FormValue';
 import FormItem from './FormItem';
 import NativeField from './NativeField';
-import createForm from './createForm';
 
 export {
     Form,
-    FormValue,
-    createForm,
     FormItem,
     NativeField,
 };
