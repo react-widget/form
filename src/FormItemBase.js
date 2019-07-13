6d9f531a79dd6fbe405936f2ce80f3d9b28d096c
bplok20010 <nobo.zhou@foxmail.com>
2019-07-13 23:42:48 +0800

    1. add FormItemBase.js

---
+++ src/FormItemBase.js
@@ -0,0 +1,57 @@
+import React from "react";
+import PropTypes from "prop-types";
+import classnames from "classnames";
+import FormContext from "./FormContext";
+export default class FormItemBase extends React.Component {
+    static contextType = FormContext;
+
+    static propTypes = {
+        name: PropTypes.string,
+        rules: PropTypes.oneOfType([
+            PropTypes.object,
+            PropTypes.array,
+            PropTypes.func
+        ]),
+        required: PropTypes.bool
+    };
+
+    static defaultProps = {};
+
+    constructor(props, ...args) {
+        super(props, ...args);
+        const { form } = this.context;
+
+        if (props.name) {
+            form.addField(this);
+        }
+    }
+
+    componentWillUnmount() {
+        const { form } = this.context;
+        form.removeField(this);
+    }
+
+    isRequired() {
+        const { form } = this.context;
+        const { name } = this.props;
+        let rules = form.getFieldRules(name);
+        let isRequired = false;
+
+        if (rules && rules.length) {
+            isRequired = rules.some(rule => rule.required);
+        }
+
+        return isRequired;
+    }
+
+    getValue() {
+        const { name } = this.props;
+        const { form } = this.context;
+
+        return form.getValue(name);
+    }
+
+    render() {
+        return null;
+    }
+}
