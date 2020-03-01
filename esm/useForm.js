4d84d0ce805ceb4062529eceee58d8c2721d3f1b
bplok20010 <nobo.zhou@foxmail.com>
2020-03-01 21:36:40 +0800

    1. 添加transform-es构建

--- esm/useForm.js
+++
@@ -1,7 +0,0 @@
-
-import React from "react";
-import FormContext from "./FormContext";
-export default (function () {
-  var context = React.useContext(FormContext);
-  return context.form;
-});
--- esm/useFormItem.js
+++
@@ -1,7 +0,0 @@
-
-import React from "react";
-import FormItemContext from "./FormItemContext";
-export default (function () {
-  var context = React.useContext(FormItemContext);
-  return context.formItem;
-});
