944796d87cdb66b8014ed2c2374ce9d9b2dacc45
bplok20010 <nobo.zhou@foxmail.com>
2020-03-18 22:13:10 +0800

    Update deps

--- packez.config.js
+++ packez.config.js
@@ -1,9 +1,1 @@
-const _ = require("lodash");
-
-module.exports = function({ method, program, ...defaults }) {
-	const opts = {
-		tsCheck: false,
-	};
-
-	return _.defaultsDeep(opts, defaults);
-};
+module.exports = function(options, state) {};
